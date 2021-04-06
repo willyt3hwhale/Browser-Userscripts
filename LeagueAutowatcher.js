0cc1db17f42111364fb50309347b1898ee04df3f
Wilhelm Åkermark <willyt3hwhale@gmail.com>
2021-04-06 19:44:02 +0200

    Update LeagueAutowatcher.js

--- LeagueAutowatcher.js
+++ LeagueAutowatcher.js
@@ -7,24 +7,24 @@ function sleep(ms) {
 }
 
 async function watch() {
-	console.log('Switching game...')
-	vods =document.getElementsByClassName('item vods')[0];
-	vods.click();
+    console.log('Switching game...')
+    vods =document.getElementsByClassName('riotbar-navbar-link')[1];
+    vods.click();
 
-	await sleep(5000);
-	console.log('Going to vods...')
-	games = document.getElementsByClassName('game');
+    await sleep(5000);
+    console.log('Going to vods...')
+    games = document.getElementsByClassName('game');
 
-	for (i = 0; i < games.length; i++) {
-	  if (!games[i].className.includes('watched') && !games[i].className.includes('not-played')) {games[i].click();break}
-	}
+    for (i = 0; i < games.length; i++) {
+      if (!games[i].className.includes('watched') && !games[i].className.includes('not-played')) {games[i].click();break}
+    }
 
-	console.log('Clicking game...')
-	await sleep(5000);
+    console.log('Clicking game...')
+    await sleep(5000);
 }
 
 function jumptostart() {
-	document.getElementsByClassName('text')[0].click();
+    document.getElementsByClassName('text')[0].click();
 }
 
 watch()
